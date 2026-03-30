f223a4f95f9530338ee737a2a3d28cea6bc9a80a
Mia1017 <wangbeiyi9@gmail.com>
2026-03-30 15:02:39 +0800

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -21,7 +21,7 @@ export default function AboutPage() {
           {/* 简历图片 */}
           <div className="w-full rounded-xl overflow-hidden border border-border shadow-sm bg-white">
             <img
-              src="/jljl.jpg"
+              src="/v0-designer-brand-tk/jljl.jpg"
               alt="简历"
               className="w-full h-auto block"
             />
